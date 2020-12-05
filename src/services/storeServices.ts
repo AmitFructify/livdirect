17b09eca8250b8d855c38fb920411c285e80a7da
Amit Parmar <amit.parmar@livspace.com>
2020-12-06 01:37:44 +0530

    state issue

--- src/services/storeServices.ts
+++ src/services/storeServices.ts
@@ -2,13 +2,13 @@ const axios = require('axios').default;
 
 const StoreServices = {
     storeList: () => {
-        return axios.get('/v1/vendors');
+        return axios.get('https://omni-hack.herokuapp.com/v1/vendors');
     },
     storeProductList: (storeId: string) => {
-        return axios.get(`/v1/vendors/${storeId}/products`);
+        return axios.get(`https://omni-hack.herokuapp.com/v1/vendors/${storeId}/products`);
     },
     storeProduct: (productId: string) => {
-        return axios.get(`/v1/products/${productId}`);
+        return axios.get(`https://omni-hack.herokuapp.com/v1/products/${productId}`);
     }
 };
 
